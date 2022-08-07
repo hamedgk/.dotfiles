2c82d67da9e63ea07188bda0ed64c634720e5fe8
hamed gandomkar <dev.hamedgk@yahoo.com>
2022-08-07 14:36:08 +0430

    add telescope nvim plugin keybindings

--- .config/nvim/lua/modules/keybinds.lua
+++ .config/nvim/lua/modules/keybinds.lua
@@ -46,3 +46,17 @@ map('x', '<C-k>', ":move '<-2<CR>gv=gv")
 -- e.g. dA = delete buffer ALL, yA = copy whole buffer ALL
 map('o', 'A', ':<C-U>normal! mzggVG<CR>`z')
 map('x', 'A', ':<C-U>normal! ggVG<CR>')
+
+
+-- Telescope Keymaps
+vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', {
+    noremap = true,
+    silent = true
+})
+
+vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<CR>', {
+    noremap = true,
+    silent = true
+})
+
+
