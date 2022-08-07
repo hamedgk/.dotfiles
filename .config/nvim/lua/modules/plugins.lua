4f2be554de9981d233087856f9cc342f342ae6e6
hamed gandomkar <dev.hamedgk@yahoo.com>
2022-08-07 14:37:03 +0430

    install telescope nvim plugin

--- .config/nvim/lua/modules/plugins.lua
+++ .config/nvim/lua/modules/plugins.lua
@@ -32,6 +32,14 @@ return require('packer').startup(function(use)
     use 'mfussenegger/nvim-dap'
     use "EdenEast/nightfox.nvim" -- Packer
 
+
+    --Telescope
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
