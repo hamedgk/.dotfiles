937553e10402e5250cfc2716b8623bbff602a8dc
hamed gandomkar <dev.hamedgk@yahoo.com>
2022-08-16 20:45:13 +0430

    fix error caused by .txt pattern in  vertical help page autocmd snippet

--- .config/nvim/lua/modules/autocmd.lua
+++ .config/nvim/lua/modules/autocmd.lua
@@ -3,17 +3,6 @@ local A = vim.api
 
 local num_au = A.nvim_create_augroup('NUMTOSTR', { clear = true })
 
--- Open help vertically and press q to exit
-A.nvim_create_autocmd('BufEnter', {
-    group = num_au,
-    pattern = '*.txt',
-    callback = function()
-        if vim.bo.buftype == 'help' then
-            A.nvim_cmd({ cmd = 'wincmd', args = { 'L' } }, {})
-            vim.keymap.set('n', 'q', '<CMD>q<CR>', { buffer = 0 })
-        end
-    end,
-})
 
 -- Highlight the region on yank
 A.nvim_create_autocmd('TextYankPost', {
