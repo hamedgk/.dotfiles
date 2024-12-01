ec654abbd31aef994d1f315738d0213372b28b12
hamed gandomkar <dev.hamedgk@yahoo.com>
2024-12-01 18:51:29 +0330

    backup arch user repo apps

--- snapshot.sh
+++ snapshot.sh
@@ -1,4 +1,5 @@
 cp -r /home/hamedgk/.config/{alacritty,sway,waybar} .config/
 cp -r /opt/{github_token,grim} opt/
 cp /home/hamedgk/.zshrc .
-pacman -Qe > arch_installed_apps.txt
+pacman -Qe > arch_repo_installed_apps.txt
+pacman -Qm > user_repo_installed_apps.txt
