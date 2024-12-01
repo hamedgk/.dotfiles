0b39cffc44f6001e57718dbe00ea0c7dbc4a6580
hamed gandomkar <dev.hamedgk@yahoo.com>
2024-12-01 19:11:48 +0330

    omit versions & deps

--- snapshot.sh
+++ snapshot.sh
@@ -1,5 +1,24 @@
+#!/bin/bash
+
 cp -r /home/hamedgk/.config/{alacritty,sway,waybar} .config/
 cp -r /opt/{github_token,grim} opt/
+
 cp /home/hamedgk/.zshrc .
-pacman -Qe > arch_repo_installed_apps.txt
-pacman -Qm > user_repo_installed_apps.txt
+
+ARCH_REPO_PACKAGES_PATH='arch_repo_installed_apps.txt'
+USER_REPO_PACKAGES_PATH='user_repo_installed_apps.txt'
+DEPEENDANCY_PACKAGES_PATH='dependency_installed_apps.txt'
+
+pacman -Qeq > $ARCH_REPO_PACKAGES_PATH
+echo $ARCH_REPO_PACKAGES_PATH
+
+pacman -Qmq > $USER_REPO_PACKAGES_PATH
+echo $USER_REPO_PACKAGES_PATH
+
+pacman -Qtdq > $DEPEENDANCY_PACKAGES_PATH
+echo $DEPEENDANCY_PACKAGES_PATH
+
+if [ -s $DEPEENDANCY_PACKAGES_PATH ]; then
+	echo "info: $DEPEENDANCY_PACKAGES_PATH is not empty"
+fi
+
